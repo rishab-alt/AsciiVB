cf06a1a3d37fd004b8d218a1760320f127a4cb96
Rish Saddiq <rishab.saddiq0607@gmail.com>
2023-12-17 21:20:43 +0000

    Add project files.

---
+++ Ascii/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        lblASCII = New Label()
+        txtInput = New TextBox()
+        btnConvert = New Button()
+        SuspendLayout()
+        ' 
+        ' lblASCII
+        ' 
+        lblASCII.AutoSize = True
+        lblASCII.Font = New Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point)
+        lblASCII.Location = New Point(29, 25)
+        lblASCII.Name = "lblASCII"
+        lblASCII.Size = New Size(183, 45)
+        lblASCII.TabIndex = 0
+        lblASCII.Text = "Ascii result"
+        ' 
+        ' txtInput
+        ' 
+        txtInput.Location = New Point(234, 317)
+        txtInput.Name = "txtInput"
+        txtInput.Size = New Size(148, 23)
+        txtInput.TabIndex = 1
+        ' 
+        ' btnConvert
+        ' 
+        btnConvert.Location = New Point(271, 355)
+        btnConvert.Name = "btnConvert"
+        btnConvert.Size = New Size(75, 23)
+        btnConvert.TabIndex = 2
+        btnConvert.Text = "Submit"
+        btnConvert.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(618, 450)
+        Controls.Add(btnConvert)
+        Controls.Add(txtInput)
+        Controls.Add(lblASCII)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lblASCII As Label
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents btnConvert As Button
+End Class
---
+++ Ascii/Form1.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Text
+
+Public Class Form1
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        ' Get the text entered into the TextBox
+        Dim userInput As String = txtInput.Text
+
+        ' Process the input and generate ASCII values
+        Dim asciiResult As String = ConvertToASCII(userInput)
+
+        ' Display the ASCII values in the Label
+        lblASCII.Text = "ASCII Values: " & vbCrLf & asciiResult
+    End Sub
+
+
+    ' Function to convert input to ASCII values
+    Private Function ConvertToASCII(inputText As String) As String
+        Dim asciiResult As New StringBuilder()
+
+        For Each character As Char In inputText
+            Dim asciiValue As Integer = AscW(character)
+            asciiResult.AppendLine($"Character: {character}, ASCII Value: {asciiValue}")
+        Next
+
+        Return asciiResult.ToString()
+    End Function
+End Class
